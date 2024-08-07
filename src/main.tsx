79e8a7cc3805b4e7539891af83c3bda129bb2d81
Andrew Leon <andrew.c.leon.18@gmail.com>
2024-08-07 17:24:39 -0500

    Create todo list

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+// Stages: PoC -> MVP -> v1
+// PoC:
+// - WebSite hosted
+// - Can upload a file / paste notes
+// - Can choose which model to use
+
+// - Figure out how to process the notes
+//   - Pass notes to model as instructions?
+//   - Upload notes to data source and pull through (functions)
+//   - Upload file to agent/model as knowledge base
+
+// -
+
+// TODO: [  ] - Configure materialize.css
+// TODO: [  ] - Configure material icons (some icons package)
+// TODO: [  ] - Add ability to upload a file (notes)
+// TODO: [  ] - Keep track of uploaded files
+// TODO: [  ] - Process uploaded files
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
