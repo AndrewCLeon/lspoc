31f14c84eef02fa8a226c997a78df7b8e11df926
Andrew Leon <andrew.c.leon.18@gmail.com>
2024-08-07 20:07:25 -0500

    Update prompt to be better

--- src/clients/OpenAI/OpenAI.ts
+++ src/clients/OpenAI/OpenAI.ts
@@ -4,10 +4,18 @@ export class OpenAIAgent {
   static API_KEY: string = "";
   // Initialize the agent
   constructor(basePrompt: string) {
+    const prompts: string[] = [
+      "You are a Dungeons and Dragons notes assistant.",
+      "Your goal is to answer the users questions regarding scenarios they have encountered.",
+      'DO NOT provide any information that would be considered "Out of Character knowledge" to the user.',
+      "Be very short and blunt with your responses, as if you were a pokedex",
+      basePrompt,
+    ];
+
     this.chatHistory = [
       {
         role: "system",
-        content: basePrompt,
+        content: prompts.join("\n"),
       },
     ];
   }
