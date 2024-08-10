b983a980a89244457bdc7c062675bc34e2a0dd5a
Andrew Leon <andrew.c.leon.18@gmail.com>
2024-08-09 20:47:36 -0500

    Create campaign card

---
+++ src/components/campaignCard/CampaignCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./CampaignCard.scss";
+
+type CampaignCardProps = {
+  title?: string;
+  description?: string;
+  image?: string;
+};
+
+export const CampaignCard: React.FC<CampaignCardProps> = (props) => {
+  const title = !props.title ? null : (
+    <span className="card-title">{props.title}</span>
+  );
+
+  const actionButton = (
+    <a className="btn-floating halfway-fab waves-effect waves-light red">
+      <i className="material-icons">4</i>
+    </a>
+  );
+
+  const bottomActions = (
+    <div className="card-action">
+      <a href="#">This is a link</a>
+    </div>
+  );
+
+  return (
+    <div className="card p-0">
+      <div className="card-image">
+        <img src={props.image} />
+        {title}
+        {actionButton}
+      </div>
+      <div className="card-content">
+        <p>{props.description}</p>
+      </div>
+      {bottomActions}
+    </div>
+  );
+};
