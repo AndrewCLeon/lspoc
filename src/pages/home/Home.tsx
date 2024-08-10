493143e078144c8fb40a36c8d06b3e9b9346fc27
Andrew Leon <andrew.c.leon.18@gmail.com>
2024-08-09 21:12:06 -0500

    More beauty cards

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,15 +6,21 @@ export const Home: React.FC = () => {
     <div className="row">
       <div className="col s4">
         <CampaignCard
-          description="The Tomb of Annihilation is a deadly temple hidden deep within a dark, overgrown jungle, where ancient traps and curses await those who dare to enter."
           image="/lspoc/TOA.webp"
+          description="The Tomb of Annihilation is a deadly temple hidden deep within a dark, overgrown jungle, where ancient traps and curses await those who dare to enter."
         />
       </div>
       <div className="col s4">
-        <div className="card"></div>
+        <CampaignCard
+          image="/lspoc/MadMage.webp"
+          description="Waterdeep: Dungeon of the Mad Mage is an ancient, labyrinthine dungeon beneath Waterdeep, filled with deadly traps, twisted monsters, and the lingering madness of its creator, Halaster Blackcloak."
+        />
       </div>
       <div className="col s4">
-        <div className="card"></div>
+        <CampaignCard
+          image="/lspoc/StormKing.webp"
+          description="Storm King’s Thunder is an epic adventure where towering giants roam the land under a raging storm, threatening to plunge the world into chaos and destruction."
+        />
       </div>
     </div>
   );
