e52dee635b713343c099eb1fb03b6424caf9bba6
Andrew Leon <andrew.c.leon.18@gmail.com>
2024-08-09 21:01:44 -0500

    Update navbar to take in and set api key

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Navbar.scss";
+import { useNavigate } from "react-router";
 
 export const Navbar: React.FC = () => {
+  const navigate = useNavigate();
+
+  const [apiKeyAvailable, setApiKeyAvailable] = React.useState<boolean>(false);
+
+  useEffect(() => {
+    const key = localStorage.getItem("API_KEY");
+    if (key) {
+      setApiKeyAvailable(true);
+    }
+  }, []);
+
+  const handleSetKey = () => {
+    const key = prompt("Enter your API key");
+    if (!key) return;
+
+    // Set the key in local storage
+    localStorage.setItem("API_KEY", key);
+    setApiKeyAvailable(true);
+  };
+
+  const setKeyContent = React.useMemo(() => {
+    if (apiKeyAvailable) return null;
+    return (
+      <li>
+        <span onClick={handleSetKey}>Set Key</span>
+      </li>
+    );
+  }, [apiKeyAvailable]);
+
+  const navigateToFiles = () => navigate("/files/upload");
+
   return (
     <div className="navbar-fixed">
       <nav>
@@ -10,6 +42,7 @@ export const Navbar: React.FC = () => {
             Lore Squire
           </a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>{setKeyContent}</li>
             <li>
               <a href="badges.html">Campaigns</a>
             </li>
@@ -17,7 +50,7 @@ export const Navbar: React.FC = () => {
               <a href="sass.html">Characters</a>
             </li>
             <li>
-              <a href="collapsible.html">Files</a>
+              <span onClick={navigateToFiles}>Files</span>
             </li>
           </ul>
         </div>
