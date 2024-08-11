0be7bc60f7d753d78321eaa3178d84ff98a8d624
Andrew Leon <andrew.c.leon.18@gmail.com>
2024-08-10 22:32:21 -0500

    Isolate campaign banner logic

---
+++ src/components/campaignBanner/CampaignBanner.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useParams } from "react-router";
+import "./CampaignBanner.scss";
+
+export const CampaignBanner: React.FC = () => {
+  const divRef = React.createRef<HTMLDivElement>();
+
+  const { campaignId } = useParams<{ campaignId: string }>();
+
+  React.useEffect(() => {
+    if (campaignId) {
+      divRef.current!.style!.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(/lspoc/${campaignId}.webp)`;
+    }
+  }, [campaignId]);
+  return (
+    <div ref={divRef} className="campaign-banner">
+      <div className="campaign-banner-text">
+        <h1>{campaignId}</h1>
+      </div>
+    </div>
+  );
+};
