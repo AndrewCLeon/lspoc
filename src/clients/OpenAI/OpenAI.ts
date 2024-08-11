c9fd3c33656856ea2a67f4a0adff68ef487e0a1f
Andrew Leon <andrew.c.leon.18@gmail.com>
2024-08-11 00:45:21 -0500

    Isolate instructions

--- src/clients/OpenAI/OpenAI.ts
+++ src/clients/OpenAI/OpenAI.ts
@@ -1,16 +1,11 @@
 import OpenAI from "openai";
 import { OpenAIBaseClient } from "./OpenAIBaseClient";
+import { assistantInstructions } from "../../instructions/AI";
 
 export class OpenAIAgent {
   // Initialize the agent
-  constructor(basePrompt: string) {
-    const prompts: string[] = [
-      "You are a Dungeons and Dragons notes assistant.",
-      "Your goal is to answer the users questions regarding scenarios they have encountered.",
-      'DO NOT provide any information that would be considered "Out of Character knowledge" to the user.',
-      "Be very short and blunt with your responses, as if you were a pokedex",
-      basePrompt,
-    ];
+  constructor(basePrompt: string = "") {
+    const prompts: string[] = [...assistantInstructions, basePrompt];
 
     this.chatHistory = [
       {
---
+++ src/instructions/AI.ts
@@ -0,0 +1,6 @@
+export const assistantInstructions: string[] = [
+  "You are a Dungeons and Dragons notes assistant.",
+  "Your goal is to answer the users questions regarding scenarios they have encountered.",
+  'DO NOT provide any information that would be considered "Out of Character knowledge" to the user.',
+  "Be very short and blunt with your responses, as if you were a pokedex",
+];
