9961883ad0267aff3422df01bacbc3270843b39d
Andrew Leon <andrew.c.leon.18@gmail.com>
2024-08-12 22:15:58 -0500

    Update imports and register character state

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,16 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { authReducer } from "./reducers/auth";
+import { authReducer } from "./slices/auth/auth";
+import { characterReducer } from "./slices/characters/characters";
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  characters: characterReducer,
 });
 
 const store = configureStore({
   reducer: rootReducer,
 });
+
+export type AppState = ReturnType<typeof store.getState>;
 
 export default store;
