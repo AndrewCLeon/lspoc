83e2c68405fe05bb6227394701f17e90e534b68a
Andrew Leon <andrew.c.leon.18@gmail.com>
2024-08-12 22:22:37 -0500

    Display characters under their campaign

--- src/pages/campaign/ViewCampaign.tsx
+++ src/pages/campaign/ViewCampaign.tsx
@@ -1,28 +1,33 @@
 import React from "react";
 import { useParams } from "react-router";
+import { useSelector } from "react-redux";
 import { CampaignBanner } from "../../components/campaignBanner/CampaignBanner";
 import { CharacterCard } from "../../components/characterCard/CharacterCard";
+import { characterSelectors } from "../../store/slices/characters/characters";
 
 export const ViewCampaign: React.FC = () => {
   const { campaignId } = useParams<{ campaignId: string }>();
+  const characters = useSelector(characterSelectors.getCharactersByCampaign)(
+    campaignId ?? ""
+  ) as Character[];
+
+  const campaignCharacterCards = React.useMemo(() => {
+    return Object.values(characters).map((character) => {
+      return (
+        <div key={character.assistantId} className="col s3">
+          <CharacterCard
+            campaignId={campaignId}
+            assistantId={character.assistantId}
+          />
+        </div>
+      );
+    });
+  }, [characters, campaignId]);
 
   return (
     <>
       <CampaignBanner />
-      <div className="row">
-        <div className="col s3">
-          <CharacterCard campaignId={campaignId} characterId="Elara" />
-        </div>
-        <div className="col s3">
-          <CharacterCard campaignId={campaignId} characterId="Elara" />
-        </div>
-        <div className="col s3">
-          <CharacterCard campaignId={campaignId} characterId="Elara" />
-        </div>
-        <div className="col s3">
-          <CharacterCard campaignId={campaignId} characterId="Elara" />
-        </div>
-      </div>
+      <div className="row">{campaignCharacterCards}</div>
     </>
   );
 };
